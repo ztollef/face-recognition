4ef37ca1a74aa89099bf4718c86d7a91fa990604
Zach Tollefson <tolle239@umn.edu>
2020-09-28 03:35:30 -0500

    Updates

---
+++ precache-manifest.16554cbc0591d8f9cd699b6a371a17e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0184cbdd4e75f3112f2af37c5c9bbb00",
+    "url": "/face-recognition/index.html"
+  },
+  {
+    "revision": "c506eb20c82b9b3a9641",
+    "url": "/face-recognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7204631e186e66e5335e",
+    "url": "/face-recognition/static/css/main.0bf65c2f.chunk.css"
+  },
+  {
+    "revision": "c506eb20c82b9b3a9641",
+    "url": "/face-recognition/static/js/2.fa2045d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-recognition/static/js/2.fa2045d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7204631e186e66e5335e",
+    "url": "/face-recognition/static/js/main.ea0cf02c.chunk.js"
+  },
+  {
+    "revision": "f324b8e0529788315b15",
+    "url": "/face-recognition/static/js/runtime-main.fcbf48af.js"
+  }
+]);
